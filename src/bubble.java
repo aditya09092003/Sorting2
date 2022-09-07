294fcff0091d49942fa76eae87ea515a7d609378
Adittiya Rao <Adittiya Rao@10.12.3.137>
2022-09-07 16:54:37 +0530

    New Sorting Algorithm

--- src/bubble.java
+++ src/bubble.java
@@ -1,4 +1,25 @@
+import java.util.*;
 
 public class bubble {
+	void Bubblesort(int arr[]) {
+		int temp;
+		int n = arr.length;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n - i - 1; j++) {
+				if (arr[j] > arr[j+1]) {
+					temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
+				}
+			}
+		}
+	}
 
+	public static void main(String[] args) {
+		bubble b = new bubble();
+		int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
+		b.Bubblesort(arr);
+		System.out.println(Arrays.toString(arr));
+	}
 }
+
